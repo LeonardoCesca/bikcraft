fa64ab1ec8a9b24749ec35d3eebab9ed52a04e35
Leonardo Flach <leonardo@dzestudio.com.br>
2018-10-31 16:17:33 -0300

    add ajusts

--- js/main.js
+++ js/main.js
@@ -10,3 +10,28 @@ $(".rslides").responsiveSlides({
     timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
     pager: true          // Boolean: Show pager, true or false
   });
+  
+Visibility.onVisible(function(){
+  setTimeout(function() {
+    $('.introduction__title').addClass('animated fadeInDown');
+  }, 400);
+  setTimeout(function() {
+    $('.quote-extern').addClass('animated fadeInDown');
+  }, 800);
+  setTimeout(function() {
+    $('.introduction .btn').addClass('animated fadeInDown');
+  }, 1200);
+  setTimeout(function() {
+    $('.animate').addClass('animated fadeInDown');
+  }, 1600);
+
+  setTimeout(function() {
+    $('.introduction-intern__title').addClass('animated fadeInDown');
+  }, 400);
+  setTimeout(function() {
+    $('.introduction-intern p').addClass('animated fadeInDown');
+  }, 800);
+  setTimeout(function() {
+    $('.intern-animate').addClass('animated fadeInDown');
+  }, 1200);
+});
